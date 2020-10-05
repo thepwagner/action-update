a6cb0e7ab62d9af9ecaccf0d8247b0d3e0c959b8
Peter Wagner <git@mycloudand.me>
2020-10-05 18:22:24 -0400

    nit: updater2 import

--- updater/branch_test.go
+++ updater/branch_test.go
@@ -5,26 +5,26 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	updater2 "github.com/thepwagner/action-update/updater"
+	"github.com/thepwagner/action-update/updater"
 )
 
 func TestDefaultUpdateBranchNamer_Format(t *testing.T) {
 	const baseBranch = "main"
-	branchNamer := updater2.DefaultUpdateBranchNamer{}
+	branchNamer := updater.DefaultUpdateBranchNamer{}
 
 	cases := []struct {
 		branch string
-		update updater2.Update
+		update updater.Update
 	}{
 		{
-			update: updater2.Update{
+			update: updater.Update{
 				Path: "github.com/foo/bar",
 				Next: "v1.2.3",
 			},
 			branch: "action-update-go/main/github.com/foo/bar/v1.2.3",
 		},
 		{
-			update: updater2.Update{
+			update: updater.Update{
 				Path:     "github.com/foo/bar/v2",
 				Previous: "v2.0.0",
 				Next:     "v3.0.0",
@@ -32,7 +32,7 @@ func TestDefaultUpdateBranchNamer_Format(t *testing.T) {
 			branch: "action-update-go/main/github.com/foo/bar/v2/v3.0.0",
 		},
 		{
-			update: updater2.Update{
+			update: updater.Update{
 				Path:     "github.com/foo/bar",
 				Previous: "v2.0.0",
 				Next:     "v3.0.0",
