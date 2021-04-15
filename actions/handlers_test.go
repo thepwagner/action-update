b06d735ac379686319e27bc42a192ceef81fee87
actions-update-go <noreply@github.com>
2021-04-15 08:03:23 +0000

    github.com/google/go-github/v34@v35.0.0

--- actions/handlers_test.go
+++ actions/handlers_test.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"testing"
 
-	"github.com/google/go-github/v34/github"
+	"github.com/google/go-github/v35/github"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/thepwagner/action-update/actions"
